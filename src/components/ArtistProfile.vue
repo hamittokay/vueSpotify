b6dbe7db5583d0bcc8843b5f9b68883dd8b9ad99
Abdullah Mara <zull.mara00@gmail.com>
2017-06-21 01:15:54 +0300

    convert indentation to spaces

--- src/components/ArtistProfile.vue
+++ src/components/ArtistProfile.vue
@@ -1,101 +1,101 @@
 <template>
-	<div>
-		<template>
-			<div class="col-md-2 col-xs-4">
-				<img class="thumb-image"
-				     :src=info.images[0].url
-				     alt="">
-			</div>
-			<div class="col-md-10 col-xs-8">
-				<h2>{{info.name}} </h2>
-				<ul class="genres"
-				    v-if="info.genres.length > 0">
-					<span>
-						<b>GENRES:</b>
-					</span>
-					<li class="genres"
-					    v-for="genre in info.genres">
-						{{genre}}
-					</li>
-				</ul>
-				<div>
-					<a target="_blank"
-					   :href="info.external_urls.spotify"
-					   class="link green">Go to Artist's Spotify Profile</a>
-					<a @click="showAlbums()"
-					   class="link">Show {{info.name}}'s albums</a>
-				</div>
-			</div>
-		</template>
-		<div class="col-md-12 albums">
-			<template v-for="album in albums">
-				<album :item="album"></album>
-			</template>
-		</div>
-	</div>
+  <div>
+    <template>
+      <div class="col-md-2 col-xs-4">
+        <img class="thumb-image"
+             :src=info.images[0].url
+             alt="">
+      </div>
+      <div class="col-md-10 col-xs-8">
+        <h2>{{info.name}} </h2>
+        <ul class="genres"
+            v-if="info.genres.length > 0">
+          <span>
+            <b>GENRES:</b>
+          </span>
+          <li class="genres"
+              v-for="genre in info.genres">
+            {{genre}}
+          </li>
+        </ul>
+        <div>
+          <a target="_blank"
+             :href="info.external_urls.spotify"
+             class="link green">Go to Artist's Spotify Profile</a>
+          <a @click="showAlbums()"
+             class="link">Show {{info.name}}'s albums</a>
+        </div>
+      </div>
+    </template>
+    <div class="col-md-12 albums">
+      <template v-for="album in albums">
+        <album :item="album"></album>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
 import Album from './Album.vue'
 
 export default {
-	name: 'ArtistProfile',
-	data() {
-		return {
-			info: {},
-			albums: {}
-		}
-	},
-	methods: {
-		showAlbums() {
-			fetch(`https://api.spotify.com/v1/artists/${this.$route.params.id}/albums`)
-				.then((res) => { return res.json() })
-				.then((albums) => { return this.albums = albums.items })
-		}
-	},
-	created() {
-		fetch(`https://api.spotify.com/v1/artists/${this.$route.params.id}`)
-			.then((res) => { return res.json() })
-			.then((res) => { return this.info = res })
-	},
-	components: { Album }
+  name: 'ArtistProfile',
+  data() {
+    return {
+      info: {},
+      albums: {}
+    }
+  },
+  methods: {
+    showAlbums() {
+      fetch(`https://api.spotify.com/v1/artists/${this.$route.params.id}/albums`)
+        .then((res) => { return res.json() })
+        .then((albums) => { return this.albums = albums.items })
+    }
+  },
+  created() {
+    fetch(`https://api.spotify.com/v1/artists/${this.$route.params.id}`)
+      .then((res) => { return res.json() })
+      .then((res) => { return this.info = res })
+  },
+  components: { Album }
 }
 </script>
 
 <style>
 a.link {
-	cursor: pointer;
-	display: inline-block;
-	padding: 15px 30px;
-	background: #333;
-	margin-right: 10px;
-	color: #fff;
-	border-radius: 40px;
-	text-align: center;
+  cursor: pointer;
+  display: inline-block;
+  padding: 15px 30px;
+  background: #333;
+  margin-right: 10px;
+  color: #fff;
+  border-radius: 40px;
+  text-align: center;
 }
 
 .thumb-image {
-	width: 100%;
-	border-radius: 100%
+  width: 100%;
+  border-radius: 100%
 }
 
 .genres {
-	display: inline-block;
+  display: inline-block;
 }
 
 .genres:after {
-	content: ',';
-	display: inline-block;
-	margin-right: 3px;
+  content: ',';
+  display: inline-block;
+  margin-right: 3px;
 }
 
 .green {
-	background: #2ebd59!important;
+  background: #2ebd59!important;
 }
 
 a:hover,
 a:active,
 a:focus {
-	text-decoration: none;
+  text-decoration: none;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,42 +1,42 @@
 <template>
-	<div>
-		<nav class="navbar navbar-inverse">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button"
-					        class="navbar-toggle collapsed"
-					        data-toggle="collapse"
-					        data-target="#navbar"
-					        aria-expanded="false"
-					        aria-controls="navbar">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<li class="navbar-brand">
-						<router-link to="/">vSpotify</router-link>
-					</li>
-				</div>
-				<div id="navbar"
-				     class="collapse navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li>
-							<router-link to="/">Home</router-link>
-						</li>
-						<li>
-							<router-link to="/about">About</router-link>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</div>
+  <div>
+    <nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button"
+                  class="navbar-toggle collapsed"
+                  data-toggle="collapse"
+                  data-target="#navbar"
+                  aria-expanded="false"
+                  aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <li class="navbar-brand">
+            <router-link to="/">vSpotify</router-link>
+          </li>
+        </div>
+        <div id="navbar"
+             class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li>
+              <router-link to="/">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/about">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 
 <script>
 export default {
-	name: 'Navbar'
+  name: 'Navbar'
 }
 </script>
